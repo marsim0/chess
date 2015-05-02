c1746e46286d21552494d163fc879d9d00db6e5d
marsim0 <marb@mail.ru>
2015-05-02 21:40:17 +0300

    Initial Commit

---
+++ chess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chess
+//
+//  Created by Мариам Б. on 02.05.15.
+//  Copyright (c) 2015 Мариам Б. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
